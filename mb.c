b5e3a4265d98518d40dd05146d1a71b092686ac6
balajimanikandanm <mkbalaji1998@gmail.com>
2017-06-28 14:27:43 +0530

    Update mb.c

--- mb.c
+++ mb.c
@@ -1,1 +1,21 @@
-bjsdlsajdcfk
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+printf("enter the number:\n");
+scanf("%d",&a);
+if(a>0)
+{
+  printf("the number is positive \n");
+}
+else if(a<0)
+{ 
+  printf("the number is negative \n");
+}
+else
+{
+  printf("it is zero \n");
+}
+  getch();
+}
