d267a1b87f26b25aa26768aa61fe477a2f435b85
balajimanikandanm <mkbalaji1998@gmail.com>
2018-08-17 12:16:51 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -8,4 +8,13 @@ ret,img=cam.read()
 gray=cv2.cvtColor(img,cv2.COLOR.BGR2GRAY)
 faces=detector.detectMultiscale(gray,1.3,5)
 for(x,y,w,h) in faces:
-cv2.rectangle(img,(x,y),(x+w,y+h))
+cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+num=num+1
+cv2.imWrite("dataSet/User."+Id+'.'+str(num)+".jpg",gray[y:y+h,x:x+w])
+cv2.imshow('frame',img)
+if cv2.WaitKey(100) & 0xFF == ord('q'):
+  break
+elif num>20:
+  break
+  cam.release()
+  cv2.destroyAllWindows()
